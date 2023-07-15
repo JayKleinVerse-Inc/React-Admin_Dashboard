26945ab912e97161fbcb04283b1c637047b17db2
Adeyemi <yemiklein77@gmail.com>
2023-07-15 23:47:50 +0100

    Piechart implementation

---
+++ src/components/barChartBox/BarChartBox.tsx
@@ -0,0 +1,38 @@
+import "./barChartBox.scss"
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+
+
+type Props = {
+    color: string;
+    icon: string;
+    title: string;
+    dataKey: string;
+    number: number | string;
+    percentage: number;
+    chartData: object[];
+  };
+
+const BarChartBox = (props: Props) => {
+  return (
+    <div className="barChartBox">
+     <h1>{props.title}</h1>
+     <div className="chart">
+     <ResponsiveContainer width="98%" height={150}>
+        <BarChart width={150} height={40} data={props.chartData}>
+        <Tooltip 
+            contentStyle={{background:"lemon", borderRadius:"5px"}}
+            labelStyle={{display: "none"}}
+            cursor={{fill:"none"}}
+            //position={{x: 10, y: 60}}
+            />
+          <Bar dataKey={props.dataKey} fill={props.color} />
+        </BarChart>
+      </ResponsiveContainer>
+     </div>
+    </div>
+  )
+}
+
+export default BarChartBox
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,9 @@
 import TopBox from "../../components/topBox/TopBox";
 import ChartBox from "../../components/chartBox/ChartBox";
-import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data";
+import BarChartBox from "../../components/barChartBox/BarChartBox";
+import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data";
 import "./home.scss" 
+import PieChartBox from "../../components/pieChartBox/PieChartBox";
 
 
 const Home = () => {
@@ -10,12 +12,12 @@ const Home = () => {
         <div className="box box1"> <TopBox /> </div>
         <div className="box box2"> <ChartBox  { ...chartBoxUser}/> </div>
         <div className="box box3"> <ChartBox { ...chartBoxProduct}/></div>
-        <div className="box box4"> </div>
+        <div className="box box4"> <PieChartBox /> </div>
         <div className="box box5"> <ChartBox { ...chartBoxConversion}/></div>
         <div className="box box6"><ChartBox { ...chartBoxRevenue}/></div>
         <div className="box box7">Box7</div>
-        <div className="box box8">Box8</div>
-        <div className="box box9">Box9</div>
+        <div className="box box8"><BarChartBox { ...barChartBoxVisit}/></div>
+        <div className="box box9"><BarChartBox { ...barChartBoxRevenue}/></div>
     </div>
   )
 }
